d69e9223d964e06147064f4f3fc61cd158ec6337
Bill Lahti <bill.lahti@gmail.com>
2011-03-04 06:10:54 -0500

    Added DashboardDemo

---
+++ DashboardDemo/src/com/wglxy/example/dash1/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.wglxy.example.dash1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_db1);
+    }
+}
